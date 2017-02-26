6d812e0561b6e23d750435853b02874b73be4da1
mavcook <mavcook@umich.edu>
2017-02-25 22:13:19 -0500

    Int

---
+++ js/app.js
@@ -0,0 +1,73 @@
+var BG_PICS = [];
+
+function getFiles(directory, callback) {
+  chrome.runtime.getPackageDirectoryEntry(function(root) {
+    root.getDirectory(directory, {create: false}, function(localesdir) {
+      var reader = localesdir.createReader();
+      // Assumes that there are fewer than 100 locales; otherwise see DirectoryReader docs
+      reader.readEntries(function(results) {
+        callback(results.map(function(de){return de.name;}).sort());
+      });
+    });
+  });
+}
+
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+function changePic(increment)
+{
+	var i = 0;
+	if (localStorage.picIdx)// convert to keys so pictures can be moved/deleted
+		i = parseInt(localStorage.picIdx);
+
+	if (i + increment < BG_PICS.length && i + increment > 0)
+		i += increment;
+	else if (i + increment < 0)
+		i = BG_PICS.length - 1;
+	else i = 0;
+
+	localStorage.picIdx =  i;
+	$('html').css('background-image', 'url(bg/' + BG_PICS[i] + ')');
+}
+
+getFiles('bg', function(data){
+	BG_PICS = data; 
+	console.log(data);
+	changePic(0);
+});
+
+$('document').ready(function(){
+	
+	// load previous settings if exist
+	if (localStorage.name)
+		$('#name').html(localStorage.name);
+	$('#nextPic').click(function(){ changePic(1) });
+	$('#prevPic').click(function(){ changePic(-1) });
+
+
+	console.log(BG_PICS);
+
+	var name = $('#name');
+	var nameInputDiv = $('#wrap-input-name');
+	var nameInput =$('#input-name');
+
+	name.dblclick(function(){
+		
+		$('#wrap-input-name').show();
+		nameInput.focus();
+	});
+	
+	nameInput.keypress(function(e){
+		if (e.which === 13)
+		{
+			localStorage.setItem('name', nameInput.val());
+			$('#name').html(nameInput.val());
+			$('#wrap-input-name').hide();
+		}
+	});
+});
