1eb03308856eb56da7d0fa346a9eb1e1acc833cb
mavcook <mavcook@umich.edu>
2017-10-22 22:59:27 -0400

    Updated packaging script

--- package.py
+++ package.py
@@ -1,11 +1,15 @@
 import subprocess as sp
 import shutil
+import os
 
+outDir = '../LivLy'
 exclude = ['.git', 'uncompressed', 'ext', '.vscode', '.gitignore', 'compressor.py', 'README.md', 'package.py']
-shutil.copytree('.' , '../LivLy' ,ignore=shutil.ignore_patterns(*exclude))
+if os.path.exists(outDir) is True:
+			shutil.rmtree(outDir)
+shutil.copytree('.' , outDir ,ignore=shutil.ignore_patterns(*exclude))
 
 # make crx
 #sp.call(['google-chrome', '--pack-extension="../Livly"', '--pack-extension-key="../LivLy.pem"'])
 
-shutil.copy('../LivLy.pem', '../LivLy')
-sp.call(['zip', '-r', '../livly.zip', '../LivLy'])
+shutil.copy('../LivLy.pem', outDir)
+sp.call(['zip', '-r', '../livly.zip', outDir])
