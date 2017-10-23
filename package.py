79c64ab16a9a3b1c27607db9b4f6387be2143163
mavcook <mavcook@umich.edu>
2017-10-22 22:34:54 -0400

    Finished credits

--- package.py
+++ package.py
@@ -7,5 +7,5 @@ shutil.copytree('.' , '../LivLy' ,ignore=shutil.ignore_patterns(*exclude))
 # make crx
 #sp.call(['google-chrome', '--pack-extension="../Livly"', '--pack-extension-key="../LivLy.pem"'])
 
-shutil.move('../LivLy.pem', '../LivLy')
+shutil.copy('../LivLy.pem', '../LivLy')
 sp.call(['zip', '-r', '../livly.zip', '../LivLy'])
